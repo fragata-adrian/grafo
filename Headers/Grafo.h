5a452cce910b0da047e634afe2026b3e3c1e21cb
fragata-adrian <fragataadrian99@gmail.com>
2022-10-19 03:02:56 -0300

    documentacion

--- Headers/Grafo.h
+++ Headers/Grafo.h
@@ -5,19 +5,27 @@
 class Grafo
 {
     private:
-        NodoG* primero;
-        int numNodos;
+        NodoG* primero; // Puntero al primer nodo del grafo
+        int numNodos; // Cantidad de nodos que contiene el grafo
     public:
+        // Constructor
         Grafo();
 
+        // Retorna el valor del atributo primero
         NodoG* obtenerPrimero();
 
+        // Cambia el valor del atributo primero
         void cambiarPrimero(NodoG* nodo);
 
+        // Hace un print por consola de los nodos y sus datos
+        // que contiene el grafo.
+        // PRE: Debe haber al menos un nodo. 
         void verNodos();
 
+        // Elimina todos los nodos y sus aristas del grafo.
         void eliminarNodos();
 
+        // Agrega un nodo al grafo
         void agregarNodo(NodoG* nodo);
 };
 
--- Headers/NodoG.h
+++ Headers/NodoG.h
@@ -8,29 +8,39 @@ class Arista;
 class NodoG
 {
     private:
-        string ciudad;
-        NodoG* siguiente;
-        Arista* aristaCabeza;
-        int numAristas = 0;
+        string ciudad; // Dato que almacena el nodo
+        NodoG* siguiente; // Puntero al nodo siguiente
+        Arista* aristaCabeza; // Puntero a la primera arista del nodo
+        int numAristas = 0; // Cantidad de aristas que contiene este nodo
     public:
-        // Constructor
+        // Constructores
         NodoG();
         NodoG(string nombreCiudad);
 
+        // Retorna el valor del atributo ciudad
         string obtenerCiudad();
 
+        // Cambia el valor del atributo ciudad
         void cambiarCiudad(string ciudad);
 
+        // Retorna el puntero a la primera arista del nodo
         Arista* obtenerAristas();
 
+        // Hace un print de los datos del nodo:
+        // la ciudad que representa y hacia donde llevan sus aristas
+        // y su distancia.
         void verDatos();
 
+        // Retorna el puntero al siguiente nodo 
         NodoG* obtenerSiguiente();
 
+        // Cambia el puntero al siguiente nodo
         void cambiarSiguiente(NodoG*);
 
+        // Agrega una nueva arista a la lista de aristas del nodo
         void agregarArista(NodoG* nodoOrigen, NodoG* nodoDestino, double distancia);
 
+        // Elimina todas las aristas pertenecientes al nodo
         void eliminarAristas();
 
 };
